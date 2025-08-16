007b0048ea5bd72bba09f58d084d43cfd0ee8589
vishalsolanki-v <vishalthakur2463@gmail.com>
2025-08-17 02:01:04 +0530

    port

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,9 @@
-import Navbar from '@/components/Navbar/page'
-import CustomCursor from '@/shared/CustomCursor'
 import React from 'react'
 
 const Layout = ({children}:Readonly<{children:React.ReactNode}>) => {
   return (
     <React.Fragment>
-    {/* <Navbar/> */}
     {children}
-    {/* <CustomCursor/> */}
     </React.Fragment>
   )
 }
