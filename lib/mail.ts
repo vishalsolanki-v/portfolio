bbd1b48c6d3f088d21f45ce32bbf52464aa1464b
vishalsolanki-anagram <vishal.solanki@anagrammedia.tech>
2025-09-02 15:50:40 +0530

    message service

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,7 +1,9 @@
 import nodemailer from "nodemailer"
 
 const transporter = nodemailer.createTransport({
-  service: "gmail", // or your email provider
+    host: "smtp.gmail.com",
+  port: 465, // 587 for TLS
+  secure: true, // true for 465, false for 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
